49361a017d64f5d3796c532c813b9175a011367f
Megan Cheng <mcheng-27@peddie.org>
2024-02-29 15:13:16 -0500

    29/2 - what am i doing

--- blackjackLab/BlackJack.java
+++ blackjackLab/BlackJack.java
@@ -17,6 +17,18 @@ public class BlackJack{
 	// 	money = new Money();
 	// }
 
+	public void initializeGame(){
+		System.out.println("Welcome to Blackjack!");
+		System.out.println("");
+		System.out.println("  BLACKJACK RULES: ");
+		System.out.println("Each player is dealt 2 cards. The dealer is dealt 2 cards with one face-up and one face-down.");
+		System.out.println("Cards are equal to their value with face cards being 10 and an Ace being 1 or 11.");
+		System.out.println("The players cards are added up for their total.");
+		System.out.println("Players “Hit” to gain another card from the deck. Players “Stay” to keep their current card total.");
+		System.out.println("Dealer “Hits” until they equal or exceed 17.");
+		System.out.println("The goal is to have a higher card total than the dealer without going over 21.");
+		System.out.println("Players win their bet if they beat the dealer. Players win 1.5x their bet if they get “Blackjack” which is 21.");
+	}
 	public static void main(String[] args){
 		Scanner keyboard = new Scanner(System.in);
 		// BlackJack game = new BlackJack();
